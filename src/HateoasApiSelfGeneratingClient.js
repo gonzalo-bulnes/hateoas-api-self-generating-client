c13c014919f7be6ad0a6f19123c8bb9a9672d995
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-10-28 00:26:44 -0300

    Partial add basic nav menu generator

--- src/HateoasApiSelfGeneratingClient.js
+++ src/HateoasApiSelfGeneratingClient.js
@@ -1,4 +1,31 @@
 HateoasApiSelfGeneratingClient = function() {
+  var AppModel = {
+      "record": {
+          "id": 1,
+          "name": ""
+      },
+      "links": [
+          {
+              "rel": "next",
+              "uri": "/records/2"
+          },
+          {
+              "rel": "index",
+              "uri": "/records"
+          }
+      ]
+  };
 
+  // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+  function AppViewModel() {
+      this.raw = ko.computed(function(){
+        return AppModel
+      });
+      this.raw = ko.observable(JSON.stringify(AppModel, null, 4));
+      this.resource = ko.observable(JSON.stringify(AppModel, null, 4));
+      this.links = ko.observableArray(AppModel.links);
+  }
+
+  // Activates knockout.js
+  ko.applyBindings(new AppViewModel());
 };
-alert('Hello!')
